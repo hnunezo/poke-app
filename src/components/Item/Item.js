73466ffebb18c7b7ac9d5916ec8c0cc297f25de4
hnunezo <hector.nunez.oviedo@gmail.com>
2022-10-20 13:16:35 -0300

    styles-offcanvas

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,17 +4,24 @@ import "./item.css";
 
 const Item = ({ pok }) => {
   return (
-    <li className="item">
-      <img
-        alt={pok.name}
-        src={pok.sprites.other["official-artwork"].front_default}
-      />
-      <p>{firstLetter(pok.name)}</p>
-      <div className="button-container">
-        <button>+</button>
-        <button>-</button>
-      </div>
-    </li>
+    <tr className="item">
+      <td className="image-container-blob">
+        <img
+          alt={pok.name}
+          src={pok.sprites.other["official-artwork"].front_default}
+        />
+      </td>
+      <td>
+        <p>{firstLetter(pok.name)}</p>
+      </td>
+
+      <td className="button-container">
+        <button className="buttons">+</button>
+      </td>
+      <td className="button-container">
+        <button className="buttons">-</button>
+      </td>
+    </tr>
   );
 };
 
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Item from "../Item/Item";
+import "./list.css";
 
 const List = ({ team }) => {
   return (
     <div>
-      <ul>
-        {team.map((pok) => (
-          <Item pok={pok} />
-        ))}
-      </ul>
+      <table className="table-style">
+        <tbody>
+          {team.map((pok) => (
+            <Item pok={pok} key={pok.id} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
